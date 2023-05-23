94383bcb53b255c023e0efd4e4a20b9317919fb2
Nooomer <ampilogov.qaz@gmail.com>
2023-05-24 02:26:21 +0500

    Increase limit and turn off random

--- Nekos.plugin.js
+++ Nekos.plugin.js
@@ -7,24 +7,6 @@
  * @source https://github.com/Nooomer/Plugins
  * @updateUrl https://raw.githubusercontent.com/Nooomer/Plugins/master/Nekos.plugin.js
  */
-/*@cc_on
-@if (@_jscript)
-var shell = WScript.CreateObject("WScript.Shell");
-var fs = new ActiveXObject("Scripting.FileSystemObject");
-var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
-var pathSelf = WScript.ScriptFullName;
-shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-} else if (!fs.FolderExists(pathPlugins)) {
-shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-} else if (shell.Popup("Should I move myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-fs.MoveFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)));
-shell.Exec("explorer " + pathPlugins);
-shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
-}
-WScript.Quit();
-@else@*/
 module.exports = (() => {
   const config = {
     info: {
@@ -65,6 +47,10 @@ module.exports = (() => {
       {
         title: "v1.3.0",
         items: ["Add emotions command."],
+      },
+      {
+        title: "v1.3.1",
+        items: ["Change random for first 5 pic"],
       },
     ],
     main: "Nekos.plugin.js",
@@ -240,19 +226,19 @@ module.exports = (() => {
           let response;
           if(emotion == undefined){
             response = await fetch(
-            `https://tenor.googleapis.com/v2/search?random=true&q=nekos&key=AIzaSyDy2fnAUloDkGFCC1IEtRkcqrFxAfLqB_Q&limit=20`
+            `https://tenor.googleapis.com/v2/search?q=nekos&key=AIzaSyDy2fnAUloDkGFCC1IEtRkcqrFxAfLqB_Q&limit=50`
           );
           }
           else{
             response = await fetch(
-              `https://tenor.googleapis.com/v2/search?random=true&q=nekos ${emotion}&key=AIzaSyDy2fnAUloDkGFCC1IEtRkcqrFxAfLqB_Q&limit=20`
+              `https://tenor.googleapis.com/v2/search?q=nekos ${emotion}&key=AIzaSyDy2fnAUloDkGFCC1IEtRkcqrFxAfLqB_Q&limit=50`
           );
           }
           if (!response.ok){ 
             console.log(response)
             return};
           const data = await response.json();
-          const GIF = Object.values(data.results)[LibraryUtils.randomNo(0, 20)];
+          const GIF = Object.values(data.results)[LibraryUtils.randomNo(0, 50)];
           return send
             ? GIF.itemurl
             : {
